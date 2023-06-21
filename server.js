f89a229d540109e9fd5ebd0f2d1c04340cfba7de
RobRipley <129440077+RobRipley@users.noreply.github.com>
2023-06-21 11:48:18 -0700

    Update server.js
    adds functionality to create user with name, location, primary instrument, link to tunebook, link to friends list

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -21,13 +21,17 @@ mongoose.connect('mongodb://localhost:27017/myapp', {
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  primaryInstrument: { type: String, required: true },
   tunebook: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tune' }],
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 });
 
 const tuneSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  composer: { type: String, required: true },
-  fileUrl: { type: String, required: true },
+  rhythm: { type: String, required: true },
+  key: { type: String, required: true },
 });
 
 const User = mongoose.model('User', userSchema);
@@ -37,11 +41,18 @@ app.use(express.json());
 
 app.post('/register', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password, name, location, primaryInstrument } = req.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new User({ username, password: hashedPassword });
+    const user = new User({
+      username,
+      password: hashedPassword,
+      name,
+      location,
+      primaryInstrument,
+    });
+
     await user.save();
 
     res.status(201).json({ message: 'User registered successfully' });
@@ -68,41 +79,64 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.post('/upload-tune', upload.single('tune'), async (req, res) => {
-  const { title, composer } = req.body;
-  const tuneFile = req.file;
+app.get('/search-tunes', async (req, res) => {
+  const { query } = req.query;
 
   try {
-    const tune = new Tune({
-      title,
-      composer,
-      fileUrl: tuneFile.path,
+    const tunes = await Tune.find({
+      $or: [
+        { title: { $regex: query, $options: 'i' } },
+        { rhythm: { $regex: query, $options: 'i' } },
+        { key: { $regex: query, $options: 'i' } },
+      ],
     });
 
-    await tune.save();
-
-    res.status(201).json({ message: 'Tune uploaded successfully' });
+    res.status(200).json({ tunes });
   } catch (error) {
     res.status(500).json({ message: 'An error occurred' });
   }
 });
 
 app.post('/add-tune-to-tunebook', async (req, res) => {
-    const { userId, tuneId } = req.body;
-  
-    try {
-      const user = await User.findById(userId);
-      const tune = await Tune.findById(tuneId);
-  
-      if (!user || !tune) {
-        res.status(404).json({ message: 'User or tune not found' });
-      } else {
-        user.tunebook.push(tune);
-        await user.save();
-  
-        res.status(200).json({ message: 'Tune added to tunebook' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'An error occurred' });
+  const { userId, tuneId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    const tune = await Tune.findById(tuneId);
+
+    if (!user || !tune) {
+      return res.status(404).json({ message: 'User or tune not found' });
     }
-  });
+
+    user.tunebook.push(tune);
+    await user.save();
+
+    res.status(200).json({ message: 'Tune added to tunebook' });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred' });
+  }
+});
+
+app.post('/add-friend', async (req, res) => {
+  const { userId, friendId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    const friend = await User.findById(friendId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    user.friends.push(friend);
+    await user.save();
+
+    res.status(200).json({ message: 'Friend added successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
